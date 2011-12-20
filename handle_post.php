27d61ef3b30c244c10f9402496f1d7a741c98159
lisawilliams <lisa@placeblogger.com>
2011-12-20 01:36:05 -0500

    Exercises for Chapter 5 of  Larry Ullman's PHP for the Web
    Ch. 5 is about handling strings.

--- handle_post.php
+++ handle_post.php
@@ -9,13 +9,26 @@ $page_title = "";
 	<title><?php print($page_title) ?></title>
 </head>
 <body>
-<?php
+<?php  // Script 5.2 - handle_post.php 
+/* This script receives five values from posting.html: 
+first_name, last_name, email, posting, submit */
 
 // error handling
 ini_set('display errors',1);  // Let me learn from my mistakes!
 error_reporting(E_ALL|E_STRICT); // Show all possible problems! 
 
-?>
+// Get the values from the $_POST array: 
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$posting = $_POST['posting'];
 
+// Create a full-name variable: 
+$name = $first_name . ' '.$last_name;
+
+// Print a message: 
+print "<div>Thank you, $name, for your posting: 
+<p>$posting</p></div>";
+
+?>
 </body>
 </html>
