a3a1064d95197ae5ab836cd2652e49ae9765aed5
lisawilliams <lisa@placeblogger.com>
2011-12-19 19:05:55 -0500

    Made calculating the total more efficient.

--- handle_calc.php
+++ handle_calc.php
@@ -1,14 +1,11 @@
-<?php
-$page_title = "Product Cost Calculator";
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<title><?php print($page_title) ?></title>
+	<title>Product Cost Calculator</title>
 	<style type="text/css" media="screen">
-	.number {font-weight: bold;} 
+		.number { font-weight: bold; }
 	</style>
 </head>
 <body>
@@ -28,39 +25,38 @@ $tax = $_POST['tax'];
 $shipping = $_POST['shipping'];
 $payments = $_POST['payments'];
 
-// Calculate the total
+// Calculate the total:
 
-$total = $price * $quantity;
-$total = $total + $shipping;
+$total = ($price * $quantity) + $shipping;
 $total = $total - $discount;
+// $total = $price * $quantity;
+// $total = $total + $shipping;
+// $total = $total - $discount;
 
-// Determine the tax;rate 
-
+// Determine the tax rate:
 $taxrate = $tax/100;
 $taxrate = $taxrate + 1;
 
 // Factor in the tax rate:
-
 $total = $total * $taxrate;
 
-// Calculate the monthly payments: 
+// Calculate the monthly payments:
+$monthly = $total / $payments;
 
-$monthly = $total/$payments;
+// apply the proper formatting
+$total = number_format($total, 2);
+$monthly = number_format($monthly, 2);
 
-// Print out the results:  
-
-print "<div>You have selected to purchase:<br />
-<span class= \"number\">$quantity</span>widget(s) at <br />
-<span class= \"number\">$price</span> price each plus a <br />
-<span class= \"number\">$shipping</span> shipping cost and <br />
-<span class= \"number\">$tax</span> percent tax rate. <br />
-After your $<span class \"number\">$discount</span> discount, the total cost is
-$<span class \"number\">$total</span>
-Divided over <span class \"number\">$payments</span> monthly payments, that would be 
-<span class \"number\">$monthly</span> each.</p></div>"
+// Print out the results:
+print "<p>You have selected to purchase:<br />
+<span class=\"number\">$quantity</span> widget(s) at <br />
+$<span class=\"number\">$price</span> price each plus a <br />
+$<span class=\"number\">$shipping</span> shipping cost and a <br />
+<span class=\"number\">$tax</span> percent tax rate.<br />
+After your $<span class=\"number\">$discount</span> discount, the total cost is 
+$<span class=\"number\">$total</span>.<br />
+Divided over <span class=\"number\">$payments</span> monthly payments, that would be $<span class=\"number\">$monthly</span> each.</p>";
 
 ?>
-
-
 </body>
 </html>
