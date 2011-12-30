a84955eaefd0cdc20ef4d00a9bd1b620d594748d
lisawilliams <lisa@placeblogger.com>
2011-12-30 17:07:51 -0500

    Redid the site login page.
    I used the book's downloadable code last night, because I kept running
    into errors I was too tired to find.  Started from scratch again today
    and kept going until I got it right.  DETERMINATION, PEOPLE!!

---
+++ site/login2.php
@@ -0,0 +1,49 @@
+<?php // Script 8.8 - login2.php  A second try at this exercise which I was too tired to do last night.
+
+// This page lets users login to the site...in theory.  
+
+// Set the title page and include the header file: 
+
+define('TITLE', 'Login');
+require('templates/header.html');
+
+// Print some introductory text 
+
+print '<h1>Login Form</h1>
+<p> Users who are logged in are just better than users who aren\'t.</p>';
+
+// Check if the form has been submitted 
+
+if (isset($_POST['submitted']))
+		
+{
+	//Handle the form
+	if((!empty($_POST['email'])) && (!empty($_POST['password'])) ) 
+					
+	{
+	if((strtolower($_POST['email']) == 'me@example.com')&&($_POST['password'] == 'testpass'))
+						
+		{  // Correct!
+			print '<p> Y\'all are logged in!</p>';
+						
+		}	else	{  // Forgot a field 
+						
+			print '<p>The submitted email address or password are incorrect.</p>';
+		}	
+						
+	} 
+						
+} else  {
+							
+	  print '<form action ="login.php" method ="post">
+	  <p>Email Address:<input type="text" name="email" size="20"></p>
+	  <p>Password:<input type="text" name="password" size="20"></p>
+	  <p><input type="submit" name="submit" value="Log In"></p>
+	  <input type="hidden" name="submitted" value="true" />
+	  </form>';
+} 
+					
+require('templates/footer.html'); // Need the footer. 		
+?>
+						
+								
