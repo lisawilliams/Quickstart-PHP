dff420e3a848b21eff8e3821b9daecb16ec03c19
lisawilliams <lisa@placeblogger.com>
2011-12-29 23:51:58 -0500

    Fixed header repetition bug.  Deleted confusing identical files.

--- site/index.php
+++ site/index.php
@@ -1,15 +1,3 @@
-<?php
-$page_title = "";
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<title><?php print($page_title) ?></title>
-</head>
-<body>
-
 <?php // Script 8.4 - index. php 
 // This is the home page for this site.  It uses templates to create the layout.
 
