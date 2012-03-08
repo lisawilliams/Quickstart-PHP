35788364b4b544f58cbf4ff3b5af05fdfb18fd79
lisawilliams <lisa@placeblogger.com>
2012-03-08 01:15:52 -0500

    Created the world's tiniest CMS.  Cool!

--- site/create_table.php
+++ site/create_table.php
@@ -16,11 +16,52 @@ $page_title = "Create a Table";
 ini_set('display errors',1);  // Let me learn from my mistakes!
 error_reporting(E_ALL|E_STRICT); // Show all possible problems! 
 
+// This script connects to the MySQL server, selects the database, and creates a table. 
 
+// Connect and select: 
+if($dbc = @mysql_connect('localhost', 'database', 'lavalamp')) 
 	
-						
-
-?>
+	{
+		// Handle the error if the database couldn't be selected: 
+		if(!@mysql_select_db('myblog', $dbc))
+			
+			{
+				print '<p style="color:red;">Could not connect 
+				to MySQL:<br />' . mysql_error($dbc) . '.</p>';
+			mysql_close();
+			$dbc = FALSE;
+	}	
+	
+	}	else	{
+	
+		// Connection failure.  
+		print '<p style = "color:red;">Could not connect to MySQL:<br />'.mysql_error().'.</p>';
+	}
+	
+	if($dbc) 
+		{ 
+			// Define the query: 
+			$query = 'CREATE TABLE entries (entry_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+			title VARCHAR(100) NOT NULL, 
+			entry TEXT NOT NULL,
+			date_entered DATETIME NOT NULL
+			)';
+			
+			// Execute the query: 
+			if(@mysql_query($query))
+				{
+					print '<p>The table has been created.</p>';
+					
+				}	else	{
+				
+					print '<p style="color:red;">Could not create the table because: <br />'.mysql_error().'.</p><p>The query being run was:'.$query.'</p>';
+				}
+	
+	// Close the connection. 			
+	mysql_close();			
+	
+	}
+	?>
 
 </body>
 </html>
