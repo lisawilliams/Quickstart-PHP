51ced76dbb460fcf0bec1abc01d0639aa50396e4
lisawilliams <lisa@placeblogger.com>
2012-03-09 01:20:23 -0500

    Hey, not all blog entries are such a great idea...so now I can delete stuff.

--- stationery.php
+++ stationery.php
@@ -1,5 +1,5 @@
 <?php
-$page_title = "";
+$page_title = " ";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -10,15 +10,14 @@ $page_title = "";
 </head>
 <body>
 
-<?php // Script number, filename
+<?php // Script number , filename
 
 // error handling
 ini_set('display errors',1);  // Let me learn from my mistakes!
 error_reporting(E_ALL|E_STRICT); // Show all possible problems! 
 
 
-	
-						
+
 
 ?>
 
